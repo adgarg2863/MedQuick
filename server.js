825b5aa8028a0fb5daeb8913885871aa14c0d21c
Milan Mundhra <milanmundhra27@gmail.com>
2022-07-01 00:55:22 +0530

    lets go

--- server.js
+++ server.js
@@ -1,33 +1,54 @@
-const express = require('express')
-const connectDB = require('./config/db')
-const { errorConverter, errorHandler } = require('./middleware/error');
-const ApiError = require('./utils/ApiError');
-const httpStatus = require('http-status')
-const app = express()
-
+const express = require("express");
+const { errorConverter, errorHandler } = require("./middleware/error");
+const ApiError = require("./utils/ApiError");
+const httpStatus = require("http-status");
+const app = express();
+const path = require("path");
 //connect database
-connectDB()
+const mongoose = require("mongoose");
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+const db = process.env.MONGODB_URL;
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, { useNewUrlParser: true });
+    console.log("MongoDB connected");
+  } catch (err) {
+    console.log(err.message);
+    //exit process with failure
+    process.exit(1);
+  }
+};
+connectDB();
 
 //init middleware
-app.use(express.json({extended: false}))
-app.get('/', (req,res) => res.send('API Running'))
+app.use(express.json({ extended: false }));
+app.get("/", (req, res) => res.send("API Running"));
 
 //routes
-app.use('/api/users', require('./routes/api/users'))
-app.use('/api/auth', require('./routes/api/auth'))
-app.use('/api/profile', require('./routes/api/profile'))
-app.use('/api/search', require('./routes/api/search'))
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/search", require("./routes/api/search"));
 
 // send back a 404 error for any unknown api request
 app.use((req, res, next) => {
-    next(new ApiError(httpStatus.NOT_FOUND, 'Not found'));
-  });
-  
-  // convert error to ApiError, if needed
-  app.use(errorConverter);
-  
-  // handle error
-  app.use(errorHandler);
-const PORT = process.env.PORT || 5000
+  next(new ApiError(httpStatus.NOT_FOUND, "Not found"));
+});
 
-app.listen(PORT, () => console.log(`server started on port ${PORT}`))
+// convert error to ApiError, if needed
+app.use(errorConverter);
+
+// handle error
+app.use(errorHandler);
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`server started on port ${PORT}`));
